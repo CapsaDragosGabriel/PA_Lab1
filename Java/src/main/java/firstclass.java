7a78f3d8722c90358cd253e701c7a49a25ee784e
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-02-21 16:55:20 +0200

    Update firstclass.java

--- Java/src/main/java/firstclass.java
+++ Java/src/main/java/firstclass.java
@@ -2,5 +2,6 @@ public class firstclass {
     public static void main(String[] args) {
         System.out.println("Hello buddy");
         System.out.println("Goodbye");
+        System.out.println("hi");
     }
 }
