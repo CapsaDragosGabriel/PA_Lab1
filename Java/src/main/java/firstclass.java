b1c5637adf5cb16ed44cb47d40de723e609318e2
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-02-21 17:11:35 +0200

    Update firstclass.java

--- Java/src/main/java/firstclass.java
+++ Java/src/main/java/firstclass.java
@@ -1,8 +1,37 @@
+import java.sql.SQLOutput;
+
 public class firstclass {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Goodbye");
-        System.out.println("hi");
-        System.out.println("HELLO AGAIN");
+        String[] languages={"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n = (int) (Math.random() * 1_000_000);
+        System.out.println(n);
+        n=n*3;
+        System.out.println(n);
+
+        String binaryString="1010";
+        int decimal=Integer.parseInt(binaryString,2);
+        System.out.println("Numarul 1010 in binar este:");
+        System.out.println(decimal);
+
+        n=n+decimal;
+        System.out.println("Numarul n+numarul binar este:");
+        System.out.println(n);
+
+        String hex="FF";
+        decimal=Integer.parseInt(hex,16);
+        System.out.println("Numarul FF hexadecimal este:");
+        System.out.println(decimal);
+
+        System.out.println("Numarul n+FF este:");
+        n=n+decimal;
+        System.out.println(n);
+
+        System.out.println("Numarul final este:");
+        n=n*6;
+        System.out.println(n);
     }
 }
+
