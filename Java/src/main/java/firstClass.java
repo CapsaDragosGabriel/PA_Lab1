4ce1550fe312bf1b517ee7666e4cf2884b9a7fbb
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-02-21 18:30:39 +0200

    good

--- Java/src/main/java/firstClass.java
+++ Java/src/main/java/firstClass.java
@@ -54,7 +54,6 @@ public class firstClass {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
         sayHello();
         String[] languages={"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
 
