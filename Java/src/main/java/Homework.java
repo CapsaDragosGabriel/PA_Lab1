02de007372cf2ed35d41ecaa11ce5328634a8251
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-02-27 13:25:06 +0200

    Update Homework.java

--- Java/src/main/java/Homework.java
+++ Java/src/main/java/Homework.java
@@ -1,2 +1,20 @@
+import java.util.Random;
+
 public class Homework {
+    public static void main(String args[]) {
+        Homework app = new Homework();
+        char alphabet[] = {'A', 'C', 'G', 'T'};
+        String word = app.createRandomWord(7, alphabet);
+        System.out.println(word);
+    }
+    private String createRandomWord(int len, char[] alphabet) {
+        StringBuilder word = new StringBuilder();
+        Random rand = new Random();
+        for (int i = 0; i < len; i++) {
+            int k = rand.nextInt(alphabet.length);
+            word.append(alphabet[k]);
+        }
+        return word.toString();
+    }
+
 }
